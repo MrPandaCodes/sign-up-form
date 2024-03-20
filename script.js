9321592e4ff3fda6b883c43f56626608bda0b6f8
mrpandacodes <omersaka@yahoo.com.tr>
2024-03-20 18:44:52 +0300

    implement password check script

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let passbox= document.querySelector("#pass");
+let pass= document.querySelector("#pass");
 let passrep = document.querySelector("#pass-check");
+let passArr = [pass, passrep];
+let passBox = document.querySelector("#passbox");
 
-function passwordCheck(){console.log("pass works")};
+function passwordCheck(){
+    if(pass.value==="" & passrep.value==="" ){
+        passArr.forEach((e)=>{
+            e.classList.add("invalid")});
+        
+    }
+    else if(pass.value===passrep.value || passrep.value===pass.value ){
+        
+        passArr.forEach((e)=>{
+            passBox.classList.remove("passfail");
+            e.classList.remove("invalid")});
+        
+    }
+    else {
+        passArr.forEach((e)=>{
+            passBox.classList.add("passfail");
+            e.classList.add("invalid")});
+        }
+    };
 
-passbox.addEventListener("input",passwordCheck);
-console.log("hellow it works", passbox, passrep);
+pass.addEventListener("input",passwordCheck);
+passArr.forEach((e)=>{addEventListener("input",passwordCheck)});
+
